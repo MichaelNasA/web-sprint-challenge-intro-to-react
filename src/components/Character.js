2d138527c4c9e8e0e5c51fd5145796a0164229ec
Michael Adamson <michaelnjadamson@outlook.com>
2023-05-06 15:51:56 -0400

    an la helped me fix the code

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+
+import React from 'react';
+
+const Character = (props) => {
+
+  return (
+    <div className="App">
+          <h2>{props.name}</h2>
+          <p>Height: {props.height}</p>
+          <p>Mass: {props.mass}</p>
+    </div>
+  );
+}
+
+export default Character;
